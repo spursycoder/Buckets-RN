72e9b920a5d55ad9250e103a63f5a2dc0a986c78
Aastle Stephno <stephno2003@gmail.com>
2024-11-11 00:31:33 +0530

    add to component for test

--- app/component.tsx
+++ app/component.tsx
@@ -1,9 +1,11 @@
 import { View, Text, StyleSheet } from "react-native";
+import CupFillAnimation from "./cupFillAnimation";
 
 export default function ComponentPage() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Component Page</Text>
+      <CupFillAnimation></CupFillAnimation>
     </View>
   );
 }
